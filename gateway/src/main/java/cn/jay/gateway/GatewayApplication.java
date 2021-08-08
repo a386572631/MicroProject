572026d5ede07a0a355e91e30694d9b16a3bf767
a386572631 <jin0201@foxmail.com>
2021-08-08 11:39:29 +0800

    网关

---
+++ gateway/src/main/java/cn/jay/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package cn.jay.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringCloudApplication
+@EnableFeignClients
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
